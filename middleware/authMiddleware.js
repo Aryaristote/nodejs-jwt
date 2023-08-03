2389f62ecdce944985236a95f7dc53682305883f
aristote <aryaristote@gmail.com>
2023-08-03 16:48:29 +0200

    loggin with facebook auth

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -8,7 +8,7 @@ const requireAuth = (req, res, next) => {
     if(token){
         jwt.verify(token, 'net ninja secret', (err, decodedToken) => {
             if(err){
-                console.log(err.message)
+                console.log(err.message) 
                 res.redirect('/login');
             }else{
                 console.log(decodedToken)
@@ -21,7 +21,7 @@ const requireAuth = (req, res, next) => {
 }
 
 //Check current user
-const checkUser = (req, res, next) => {
+const checkUser = (req, res, next) => {  
     const token = req.cookies.jwt;
 
     //Checking if the web token exist & valide
